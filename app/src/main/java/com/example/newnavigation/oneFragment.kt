51e98e03a8872508f38c8f24279e6a68c9f61f64
TamirisRC <tamiris.carvalho5@etec.sp.gov.br>
2022-06-23 14:06:28 -0300

    Troca de telas

---
+++ app/src/main/java/com/example/newnavigation/oneFragment.kt
@@ -0,0 +1,27 @@
+package com.example.newnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class oneFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_one, container, false)
+        view.findViewById<Button>(R.id.btn2).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_oneFragment_to_twoFragment)
+        }
+
+        view.findViewById<Button>(R.id.btn3).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_oneFragment_to_threeFragment)
+        }
+        return view
+    }
+    }
